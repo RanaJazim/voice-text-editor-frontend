e35e38c955ee1a5267b09b793e2d1cc413b79497
JazimAbbas <stackdeveloper87@gmail.com>
2020-12-29 01:04:05 +0500

    change the layout of the editor screen ..

--- src/screens/editor_screen.js
+++ src/screens/editor_screen.js
@@ -12,20 +12,61 @@ const EditorScreen = () => {
 
   return (
     <div className="container mt-4">
-      <div className="row">
-        <div className="col-md-7">
-          <div className="row">
-            <div className="col-md-12">
-              <SelectLanguage language={language} onChangeLang={handleChange} />
-            </div>
-          </div>
-          <div className="mt-3">
-            <Editor language={language} />
-          </div>
-        </div>
-      </div>
+      <BottomRightLayout language={language} handleChange={handleChange} />
+      {/* <ParallelLayout language={language} handleChange={handleChange} /> */}
+      {/* <OriginalLayout language={language} handleChange={handleChange} /> */}
     </div>
   );
 };
 
 export default EditorScreen;
+
+function BottomRightLayout({ language, handleChange }) {
+  return (
+    <div className="row">
+      <div className="col-md-12">
+        <div className="row">
+          <div className="col-md-7">
+            <Editor language={language} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-7"></div>
+          <div className="col-md-4 offset-md-1 mt-5">
+            <SelectLanguage language={language} onChangeLang={handleChange} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ParallelLayout({ language, handleChange }) {
+  return (
+    <div className="row">
+      <div className="col-md-7">
+        <Editor language={language} />
+      </div>
+      <div className="col-md-5">
+        <SelectLanguage language={language} onChangeLang={handleChange} />
+      </div>
+    </div>
+  );
+}
+
+function OriginalLayout({ language, handleChange }) {
+  return (
+    <div className="row">
+      <div className="col-md-7">
+        <div className="row">
+          <div className="col-md-12">
+            <SelectLanguage language={language} onChangeLang={handleChange} />
+          </div>
+        </div>
+        <div className="mt-3">
+          <Editor language={language} />
+        </div>
+      </div>
+    </div>
+  );
+}
