b464b0df6ed65c62901a3d279f6bbd01d70903b2
Jazim Abbas <stackdeveloper87@gmail.com>
2020-12-28 20:18:57 +0500

    fixing the bug and clear cache ..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import EditorScreen from "./screens/editor_screen";
-import SampleScreen from "./screens/sample_screen";
 
 function App() {
   return (
--- src/screens/sample_screen.js
+++ src/screens/sample_screen.js
@@ -1,15 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 
 const SampleScreen = () => {
   const { transcript, resetTranscript } = useSpeechRecognition();
-  const [error, setError] = useState(null);
-
-  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-    return null;
-  }
 
   const handleStartListening = () => {
     SpeechRecognition.startListening({ continuous: true });
@@ -20,7 +15,6 @@ const SampleScreen = () => {
       <button onClick={handleStartListening}>Start</button>
       <button onClick={SpeechRecognition.stopListening}>Stop</button>
       <button onClick={resetTranscript}>Reset</button>
-      {error && <p>This browser doesn't supported voice feature ..</p>}
       <p>{transcript}</p>
     </div>
   );
